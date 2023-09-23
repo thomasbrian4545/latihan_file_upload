b822e9dfdb30638303d6ceb253c562dc0246d031
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-23 21:24:54 +0700

    Validasi file upload

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -14,20 +14,26 @@ class FileUploadController extends Controller
     public function prosesFileUpload(Request $request)
     {
         // dump($request->berkas);
-        if ($request->hasFile('berkas')) {
-            echo "path(): " . $request->berkas->path();
-            echo "<br>";
-            echo "extension(): " . $request->berkas->extension();
-            echo "<br>";
-            echo "getClientOriginalExtension(): " . $request->berkas->getClientOriginalExtension();
-            echo "<br>";
-            echo "getMimeType(): " . $request->berkas->getMimeType();
-            echo "<br>";
-            echo "getClientOriginalName(): " . $request->berkas->getClientOriginalName();
-            echo "<br>";
-            echo "getSize(): " . $request->berkas->getSize();
-        } else {
-            echo "Tidak ada berkas yang diupload";
-        }
+
+        // if ($request->hasFile('berkas')) {
+        //     echo "path(): " . $request->berkas->path();
+        //     echo "<br>";
+        //     echo "extension(): " . $request->berkas->extension();
+        //     echo "<br>";
+        //     echo "getClientOriginalExtension(): " . $request->berkas->getClientOriginalExtension();
+        //     echo "<br>";
+        //     echo "getMimeType(): " . $request->berkas->getMimeType();
+        //     echo "<br>";
+        //     echo "getClientOriginalName(): " . $request->berkas->getClientOriginalName();
+        //     echo "<br>";
+        //     echo "getSize(): " . $request->berkas->getSize();
+        // } else {
+        //     echo "Tidak ada berkas yang diupload";
+        // }
+
+        $request->validate([
+            'berkas' => 'required|file|mimes:pdf,png|max:1000',
+        ]);
+        echo $request->berkas->getClientOriginalName() . " Lolos Validasi";
     }
 }

95e028610b51cb39a4932198c3d1875e876ba792
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-23 13:35:32 +0700

    Membuat 2 method di FileUploadController
    fileUpload()
    prosesFileUpload(Request $request)

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class FileUploadController extends Controller
 {
-    //
+    public function fileUpload()
+    {
+        return view('file-upload');
+    }
+
+    public function prosesFileUpload(Request $request)
+    {
+        return "Pemrosesan file upload di sini";
+    }
 }
